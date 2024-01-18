63c357e4860ea1fd4546c09091aadbaa38ad7d66
OrionDooms <orion45.dd@gmail.com>
2024-01-18 09:46:12 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,35 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - function sorts an array of integers
+ * using the Bubble sort algorithm.
+ * @array: Takes in the array of integers.
+ * @size: takes in the size of the array.
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t s = 0;
-	int i = 0;
+	size_t f, s = 0;
+	int j, i = 0;
 
-	printf("checking order\n");
 	while (s < size - 1)
 	{
-		if (array[i] > array[i + 1])
+		f = 0, j = 0;
+		while (f < size - s - 1)
 		{
-			swap(&array[i], &array[i + 1]);
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+			f++, j++;
 		}
-		s++;
-		i++;
+		s++, i++;
 	}
-	printf("\n");
-	printf("end");
 }
-
+/**
+ * swap - A function that swap two integers.
+ * @a: Takes in a integer.
+ * @b: Takes in a integer.
+ */
 void swap(int *a, int *b)
 {
 	int temp = *a;
