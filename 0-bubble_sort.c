3d89ba20cd5fbefba30a339ee423ca19cedd5ae5
OrionDooms <orion45.dd@gmail.com>
2024-01-18 08:28:57 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,28 @@
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size)
+{
+	size_t s = 0;
+	int i = 0;
+
+	printf("checking order\n");
+	while (s < size - 1)
+	{
+		if (array[i] > array[i + 1])
+		{
+			swap(&array[i], &array[i + 1]);
+		}
+		s++;
+		i++;
+	}
+	printf("\n");
+	printf("end");
+}
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
